0018f2cfd4b4a0091f9e04cc73085f1cbaae0525
Manoel <candidomanoel78@gmail.com>
2025-12-01 18:47:29 -0300

    feat: cria view para listar todos os alunos

--- students/views.py
+++ students/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Student
 
-# Create your views here.
+
+class StudentListView(ListView):
+    model = Student
+    template_name = 'student_list.html'
+    context_object_name = 'students'
