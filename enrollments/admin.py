5651ca6b9b604921afd14a7c1d0e3a6e6635692f
Manoel <candidomanoel78@gmail.com>
2025-12-01 17:18:59 -0300

    feat: registra model enrollment no admin

--- enrollments/admin.py
+++ enrollments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Enrollment
 
-# Register your models here.
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'enrollment_date', 'status')
+    search_fields = ('student', 'course')
