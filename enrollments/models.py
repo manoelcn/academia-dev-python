b28dd76789d2a1d3719b3af536b4c6fa85fd4756
Manoel <candidomanoel78@gmail.com>
2025-12-01 17:17:47 -0300

    feat: cria o model de enrollments

--- enrollments/models.py
+++ enrollments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from courses.models import Course
+from students.models import Student
 
-# Create your models here.
+
+class Enrollment(models.Model):
+    STATUS_CHOICE = [
+        ('pago', 'Pago'),
+        ('pendente', 'Pendente'),
+    ]
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='matriculas')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='matriculas')
+    enrollment_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='pendente')
+
+    def __str__(self):
+        return f"{self.student.name} - {self.course.name}"
