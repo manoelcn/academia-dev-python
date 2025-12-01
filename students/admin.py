96d24792ce36392f8ef58893b52cafbacb459876
Manoel <candidomanoel78@gmail.com>
2025-12-01 16:32:54 -0300

    feat: registra model student no admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'cpf', 'admission_date')
+    search_fields = ('name', 'email', 'cpf')
+    list_filter = ('admission_date',)
