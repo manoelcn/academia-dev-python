1cb8c410f9af54fc3e23f00210d8c1681b37b035
Manoel <candidomanoel78@gmail.com>
2025-12-01 16:49:33 -0300

    feat: cria models de courses

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    duration_hours = models.IntegerField()
+    enrollment_fee = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
