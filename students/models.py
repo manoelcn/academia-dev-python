cdf0c569836a13157834c04af2931359ead9ffe3
Manoel <candidomanoel78@gmail.com>
2025-12-01 16:21:27 -0300

    feat: cria o model de students

--- students/models.py
+++ students/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(unique=True)
+    cpf = models.CharField(unique=True, max_length=11)
+    admission_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
