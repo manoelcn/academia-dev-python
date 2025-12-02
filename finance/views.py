3865658a8d7f343dea2af9482c24a8fbb0ab518f
Manoel <candidomanoel78@gmail.com>
2025-12-02 15:25:17 -0300

    feat: cria view para buscar matrículas por status

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from courses.models import Course
+from enrollments.models import Enrollment
+from students.models import Student
+from django.views.generic import ListView
 
-# Create your views here.
+
+def finance(request):
+    return render(request, 'finance.html', {})
+
+def enrollments_by_status(request):
+    status = request.GET.get('status')
+    options = ['pago', 'pendente']
+    if status in options:
+        enrollments = Enrollment.objects.filter(status=status)
+    else:
+        enrollments = Enrollment.objects.all()
+    context = {'enrollments': enrollments}
+    return render(request, 'enrollments_by_status.html', context)
