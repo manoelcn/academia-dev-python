5ba65a182ebe2f90af295918b0be065ac1f98a17
Manoel <candidomanoel78@gmail.com>
2025-12-01 21:14:05 -0300

    feat: cria view para listar matrículas

--- enrollments/views.py
+++ enrollments/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Enrollment
 
-# Create your views here.
+
+class EnrollmentListView(ListView):
+    model = Enrollment
+    template_name = 'enrollment_list.html'
+    context_object_name = 'enrollments'
