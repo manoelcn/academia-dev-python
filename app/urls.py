309963c33120992a64d258ac48767566998fc448
Manoel <candidomanoel78@gmail.com>
2025-12-02 15:27:24 -0300

    feat: inclui rotas de finance

--- app/urls.py
+++ app/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('students.urls')),
     path('', include('courses.urls')),
     path('', include('enrollments.urls')),
+    path('', include('finance.urls')),
 ]
