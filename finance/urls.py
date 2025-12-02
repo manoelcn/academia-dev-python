35ec5593d7d1407ca00e5192904325a15e67601b
Manoel <candidomanoel78@gmail.com>
2025-12-02 16:08:29 -0300

    feat: cria url para acessar dados financeiros do aluno

--- finance/urls.py
+++ finance/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import enrollments_by_status
+from .views import enrollments_by_status, get_financial_status
 
 urlpatterns = [
     path('finance/enrollments/', enrollments_by_status, name='finance-enrollments'),
+    path('finance/<int:pk>/', get_financial_status , name='finance-student'),
 ]
