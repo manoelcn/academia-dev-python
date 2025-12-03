ac096f0aa7128983a6e9f7db9aa38ecc8ce092bb
Manoel <candidomanoel78@gmail.com>
2025-12-03 09:53:04 -0300

    feat: cria parâmetros para o dashboard geral

--- app/views.py
+++ app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from courses.models import Course
+from enrollments.models import Enrollment
+from students.models import Student
 
 
 def home_page(request):
-    return render(request, 'home.html', {})
+    total_student = Student.objects.count()
+    active_courses = Course.objects.filter(status=True).count()
+    paid_enrollments = Enrollment.objects.filter(status='pago').count()
+    pending_enrollments = Enrollment.objects.filter(status='pendente').count()
+    context = {
+        'total_student': total_student,
+        'active_courses': active_courses,
+        'paid_enrollments': paid_enrollments,
+        'pending_enrollments': pending_enrollments
+    }
+    return render(request, 'home.html', context)
