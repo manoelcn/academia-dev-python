7fdba366590a55f8080432e5e30f5e027c7ce1ab
Manoel <candidomanoel78@gmail.com>
2025-12-03 11:49:40 -0300

    feat: cria views de cursos para a api

--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Course
+from rest_framework import generics
+from courses.models import Course
+from courses.serializers import CourseSerializer
 
 
 class CourseListView(ListView):
     model = Course
     template_name = 'course_list.html'
     context_object_name = 'courses'
+
+class CourseCreateListAPIView(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class CourseRetriveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
