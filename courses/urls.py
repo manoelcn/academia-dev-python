ce2ca2bab408362cefd892f2a275a2cd53d07102
Manoel <candidomanoel78@gmail.com>
2025-12-03 11:50:08 -0300

    feat: cria rotas de cursos para a api

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CourseListView
+from .views import CourseListView, CourseCreateListAPIView, CourseRetriveUpdateDestroyAPIView
 
 
 urlpatterns = [
     path('courses/list/', CourseListView.as_view(), name='course-list'),
+
+    path('api/v1/courses/', CourseCreateListAPIView.as_view(), name='course-create-list-api'),
+    path('api/v1/courses/<int:pk>/', CourseRetriveUpdateDestroyAPIView.as_view(), name='course-detail-api'),
 ]
