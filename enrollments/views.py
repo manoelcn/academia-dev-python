93d88e959fe15016376c1ddaffc141a346b30d15
Manoel <candidomanoel78@gmail.com>
2025-12-03 12:03:09 -0300

    feat: cria view de create e list de matrículas para a api

--- enrollments/views.py
+++ enrollments/views.py
@@ -1,8 +1,14 @@
 from django.views.generic import ListView
-from .models import Enrollment
+from rest_framework import generics
+from enrollments.models import Enrollment
+from enrollments.serializers import EnrollmentSerializer
 
 
 class EnrollmentListView(ListView):
     model = Enrollment
     template_name = 'enrollment_list.html'
     context_object_name = 'enrollments'
+
+class EnrollmentCreateListAPIView(generics.ListCreateAPIView):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
