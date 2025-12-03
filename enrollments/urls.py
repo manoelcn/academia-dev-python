9122c4fabe577062b4d325a4cf74cd3adf542c3e
Manoel <candidomanoel78@gmail.com>
2025-12-03 12:25:49 -0300

    feat: cria endpoint de retrive e update de matrículas para api

--- enrollments/urls.py
+++ enrollments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from enrollments.views import EnrollmentListView, EnrollmentCreateListAPIView
+from enrollments.views import EnrollmentListView, EnrollmentCreateListAPIView, EnrollmentRetriveUpdateAPIView
 
 
 urlpatterns = [
     path('enrollments/list/', EnrollmentListView.as_view(), name='enrollment-list'),
 
     path('api/v1/enrollments/', EnrollmentCreateListAPIView.as_view(), name='enrollment-create-list-api'),
+    path('api/v1/enrollments/<int:pk>/', EnrollmentRetriveUpdateAPIView.as_view(), name='enrollment-detail-api'),
 ]
