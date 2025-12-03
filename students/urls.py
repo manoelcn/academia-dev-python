5659ef767a2fc15aaff7f09b875c0ec2447e84b3
Manoel <candidomanoel78@gmail.com>
2025-12-03 11:36:49 -0300

    feat: cria rotas da api

--- students/urls.py
+++ students/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('students/list/', views.StudentListView.as_view(), name='student-list'),
     path('students/<int:pk>/detail/', views.StudentDetailView.as_view(), name='student-detail'),
+
+    path('api/v1/students/', views.StudentCreateListAPIView.as_view(), name='student-create-list-api'),
+    path('api/v1/students/<int:pk>/', views.StudentRetriveUpdateDestroyAPIView.as_view(), name='student-detail-api'),
 ]
