3f86699c6f18fe37837e85dd707493510318c3d4
Manoel <candidomanoel78@gmail.com>
2025-12-03 12:03:40 -0300

    feat: cria endpoint de matrículas

--- enrollments/urls.py
+++ enrollments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import EnrollmentListView
+from enrollments.views import EnrollmentListView, EnrollmentCreateListAPIView
 
 
 urlpatterns = [
     path('enrollments/list/', EnrollmentListView.as_view(), name='enrollment-list'),
+
+    path('api/v1/enrollments/', EnrollmentCreateListAPIView.as_view(), name='enrollment-create-list-api'),
 ]
