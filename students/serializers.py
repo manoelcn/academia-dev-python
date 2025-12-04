c9735a376227d1b50c1b3ee4fddc414005980196
Manoel <candidomanoel78@gmail.com>
2025-12-04 20:09:43 -0300

    feat: cria serializer para view de matrículas de um estudante

--- students/serializers.py
+++ students/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from students.models import Student
+from enrollments.models import Enrollment
+from courses.serializers import CourseSerializer
 
 
 class StudentSerializer(serializers.ModelSerializer):
@@ -10,3 +12,10 @@ class StudentSerializer(serializers.ModelSerializer):
 class StudentReportSerializer(serializers.Serializer):
     student_name = serializers.CharField()
     total_pending = serializers.DecimalField(max_digits=10, decimal_places=2)
+
+class StudentEnrollmentsSerializer(serializers.ModelSerializer):
+    course = CourseSerializer(read_only=True)
+
+    class Meta:
+        model = Enrollment
+        fields = '__all__'
