7b55d1d9c28b30ef36e798c1a5638a3e26da1b14
Manoel <candidomanoel78@gmail.com>
2025-12-04 18:16:10 -0300

    feat: cria serializer para view de relatório

--- students/serializers.py
+++ students/serializers.py
@@ -6,3 +6,7 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+
+class StudentReportSerializer(serializers.Serializer):
+    student_name = serializers.CharField()
+    total_pending = serializers.DecimalField(max_digits=10, decimal_places=2)
